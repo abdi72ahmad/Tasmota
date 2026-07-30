7596202c31b02864e2fec58318cccbe472d26df9
abdi72ahmad <abdi72ahmad@gmail.com>
2026-07-31 00:12:24 +0700

    Update xdrv_99_btconsole.ino

--- tasmota/tasmota_xdrv_driver/xdrv_99_btconsole.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_99_btconsole.ino
@@ -10,13 +10,14 @@ bool Xdrv99(uint32_t function)
   {
     case FUNC_INIT:
       BtSerial.begin("Tasmota-BT");
+      AddLog(LOG_LEVEL_INFO, PSTR("BT: Started"));
       break;
 
     case FUNC_LOOP:
-      if (BtSerial.available())
+      while (BtSerial.available())
       {
         char c = BtSerial.read();
-        AddLog(LOG_LEVEL_INFO, PSTR("BT: %c"), c);
+        AddLog(LOG_LEVEL_INFO, PSTR("BT RX: %c"), c);
       }
       break;
   }
