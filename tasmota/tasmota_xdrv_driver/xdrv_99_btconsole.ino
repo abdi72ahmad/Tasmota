a3ba10cbf903a9348bdb5bc335f78f50492aa09a
abdi72ahmad <abdi72ahmad@gmail.com>
2026-07-31 12:17:53 +0700

    Update xdrv_99_btconsole.ino

--- tasmota/tasmota_xdrv_driver/xdrv_99_btconsole.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_99_btconsole.ino
@@ -4,6 +4,22 @@
 
 BluetoothSerial BtSerial;
 
+char BtBuffer[256];
+uint16_t BtIndex = 0;
+
+void BtConsoleSend(const char* msg)
+{
+  if (BtSerial.hasClient()) {
+    BtSerial.println(msg);
+  }
+}
+
+bool BtConsoleConnected(void)
+{
+  return BtSerial.hasClient();
+}
+
+
 bool Xdrv99(uint32_t function)
 {
   switch (function)
@@ -13,12 +29,46 @@ bool Xdrv99(uint32_t function)
       AddLog(LOG_LEVEL_INFO, PSTR("BT: Started"));
       break;
 
+
     case FUNC_LOOP:
+
       while (BtSerial.available())
       {
         char c = BtSerial.read();
-        AddLog(LOG_LEVEL_INFO, PSTR("BT RX: %c"), c);
+
+        // command finished
+        if ((c == '\r') || (c == '\n'))
+        {
+          if (BtIndex > 0)
+          {
+            BtBuffer[BtIndex] = 0;
+
+            AddLog(LOG_LEVEL_INFO, PSTR("BT CMD: %s"), BtBuffer);
+
+            ExecuteCommand(BtBuffer, SRC_SERIAL);
+
+            BtIndex = 0;
+          }
+        }
+
+        // backspace
+        else if (c == '\b')
+        {
+          if (BtIndex > 0) {
+            BtIndex--;
+          }
+        }
+
+        // store character
+        else
+        {
+          if (BtIndex < sizeof(BtBuffer) - 1)
+          {
+            BtBuffer[BtIndex++] = c;
+          }
+        }
       }
+
       break;
   }
 
